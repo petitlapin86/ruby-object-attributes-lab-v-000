4ed92a87a9d037f07227db583127aaf1346c2023
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-30 17:31:15 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
 
+
+  def name=(dog_name) # this is an instance method that takes an argument
+   @this_dogs_name = dog_name # Im setting my instance to equal a Variable
+ end
+
+ def name #this method is reporting or reading the name AKA GETTER METHOD
+    @this_dogs_name #this is an instance variable
+  end
 end 
