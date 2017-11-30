342561ba1aeae5aaace4476723605783523482d8
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-30 17:38:50 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,18 @@
 class Person
-
-
-  def name=(dog_name) # this is an instance method that takes an argument
-   @this_dogs_name = dog_name # Im setting my instance to equal a Variable
+  def name=(person_name) # this is an instance method that takes an argument
+   @name = person_name # Im setting my instance to equal a Variable
  end
 
  def name #this method is reporting or reading the name AKA GETTER METHOD
-    @this_dogs_name #this is an instance variable
+    @name #this is an instance variable
   end
-end
 
-fido = Dog.new # I am new objects being added to my class
-fido.name = "Fido"
+  def job=(person_job) # this is an instance method that takes an argument
+   @job = person_job # Im setting my instance to equal a Variable
+ end
+
+ def job #this method is reporting or reading the name AKA GETTER METHOD
+    @job #this is an instance variable
+  end
+end 
+
