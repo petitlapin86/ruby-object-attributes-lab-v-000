3310aec8a4ee4b07ad4f437301b74a6392b845c3
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-30 17:37:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,4 +6,12 @@ class Dog
  def name #this method is reporting or reading the name AKA GETTER METHOD
     @name #this is an instance variable
   end
+
+  def breed=(dog_breed) # this is an instance method that takes an argument
+   @breed = dog_breed # Im setting my instance to equal a Variable
+ end
+
+ def breed #this method is reporting or reading the name AKA GETTER METHOD
+    @breed #this is an instance variable
+  end
 end
